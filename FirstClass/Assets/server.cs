dd28191d2bc620921ebfa0ee611ad62b7ba960e0
Apple <Apple@AppledeMacBook-Air.local>
2017-08-26 18:14:51 +0800

    目标 ：2人联机塔防demo

---
+++ FirstClass/Assets/server.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net.Sockets;
+
+using UnityEngine;
+
+public class server : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		TcpClient tcpClient = new TcpClient ();
+		tcpClient.Connect ("192.168.1.101", 8888);
+		Debug.Log ("hello");
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
